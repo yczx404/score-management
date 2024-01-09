b24cc36d4ab567823fa4bb153ebacc31eda06f07
Administrator <18435947315@139.com>
2024-01-09 09:24:19 +0800

    unit test and test coverage

---
+++ src/test/java/org/example/ScoreControllerTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ScoreControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        String res = restTemplate.postForObject("http://localhost:" + port + "/statistic?cid=1", null, String.class);
+        Assert.assertNotNull(res);
+    }
+}
